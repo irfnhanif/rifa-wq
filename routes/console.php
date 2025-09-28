07bc307b0f0e52c695b6531019cf05a3a0a5fc05
irfnhanif <14.irfanh@gmail.com>
2025-09-28 12:52:36 +0700

    feat: schedule daily jobs for generating and deleting notifications

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Jobs\DeleteNotifications;
+use App\Jobs\GenerateNotifications;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new GenerateNotifications)->dailyAt('06.00');
+Schedule::job(new DeleteNotifications)->dailyAt('05.55');
