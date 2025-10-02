7dc9d62cc2a7a8a93fa4aa233aa3581f4341ca26
irfnhanif <14.irfanh@gmail.com>
2025-10-02 13:18:57 +0700

    feat: add EnsureUserRole middleware and apply role-based access control to work order routes

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', function () {
         return redirect()->route('work-orders.index');
     })->name('home');
-    
+
     Route::get('/work-orders', [WorkOrderController::class, 'index'])->name('work-orders.index');
     Route::get('/work-orders/{id}', [WorkOrderController::class, 'show'])->name('work-orders.show');
-    Route::post('/work-orders', [WorkOrderController::class, 'store'])->name('work-orders.store');
-    Route::put('/work-orders/{id}', [WorkOrderController::class, 'update'])->name('work-orders.update');
-    Route::patch('/work-orders/{id}', [WorkOrderController::class, 'markAsDone'])->name('work-orders.markAsDone');
-    Route::delete('/work-orders/{id}', [WorkOrderController::class, 'destroy'])->name('work-orders.destroy');
+
+    Route::middleware('role:USER')->group(function() {
+        Route::post('/work-orders', [WorkOrderController::class, 'store'])->name('work-orders.store');
+        Route::put('/work-orders/{id}', [WorkOrderController::class, 'update'])->name('work-orders.update');
+        Route::patch('/work-orders/{id}', [WorkOrderController::class, 'markAsDone'])->name('work-orders.markAsDone');
+        Route::delete('/work-orders/{id}', [WorkOrderController::class, 'destroy'])->name('work-orders.destroy');
+    });
 
     Route::patch('/notifications', [NotificationController::class, 'markAllAsRead'])->name('notifications.markAllAsRead');
     Route::patch('/notifications/{id}', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
