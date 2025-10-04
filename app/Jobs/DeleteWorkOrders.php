9345c50cefa1db33d7953681851bfff6a2389118
irfnhanif <14.irfanh@gmail.com>
2025-10-04 18:43:15 +0700

    feat: add job to delete work orders older than 30 days

---
+++ app/Jobs/DeleteWorkOrders.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\WorkOrder;
+use Carbon\Carbon;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class DeleteWorkOrders implements ShouldQueue
+{
+    use Queueable;
+
+    protected $signature = 'job:delete-work-orders';
+    protected $description = 'Delete work orders older than 30 days';
+
+    public function handle(): void
+    {
+        $thirtyDaysAgo = Carbon::today()->subDays(30);
+        WorkOrder::where('created_at', '>=', $thirtyDaysAgo)->forceDelete();
+    }
+}
