0d75765764e7a02b4f9d74f474d6e44251bc5ee4
irfnhanif <14.irfanh@gmail.com>
2025-10-17 07:34:18 +0700

    feat: update GenerateNotifications job schedule to 07:38 with logging for job status

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use App\Jobs\DeleteNotifications;
 use App\Jobs\DeleteWorkOrders;
 use App\Jobs\GenerateNotifications;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new DeleteNotifications)->dailyAt('04:55');
@@ -10,4 +11,7 @@ Schedule::job(new DeleteWorkOrders)->dailyAt('04:55');
 Schedule::job(new GenerateNotifications)->dailyAt('05:00');
 
 // temp
-Schedule::job(new GenerateNotifications)->dailyAt('07:30');
+Schedule::job(new GenerateNotifications)->dailyAt('07:38')
+->before(fn() => Log::info('GenerateNotifications job starting'))
+    ->onSuccess(fn() => Log::info('GenerateNotifications job completed'))
+    ->onFailure(fn() => Log::error('GenerateNotifications job failed'));
