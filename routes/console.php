943e5cff3fdae6369e56ecf71baa736354a83bd4
irfnhanif <14.irfanh@gmail.com>
2025-10-17 07:25:49 +0700

    feat: add temporary scheduled job for GenerateNotifications at 07:30

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,6 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::job(new DeleteNotifications)->dailyAt('04:55');
 Schedule::job(new DeleteWorkOrders)->dailyAt('04:55');
 Schedule::job(new GenerateNotifications)->dailyAt('05:00');
+
+// temp
+Schedule::job(new GenerateNotifications)->dailyAt('07:30');
